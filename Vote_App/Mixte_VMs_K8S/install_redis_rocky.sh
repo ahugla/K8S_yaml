5576a1351ddd19654cdde1fcbc9d14d84ba1fee9
ahugla <ahugla@vmware.com>
2023-03-20 20:19:47 +0100

    Ajout Redis en VM

---
+++ Vote_App/Mixte_VMs_K8S/install_redis_rocky.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+
+#dnf module list redis
+dnf -y install redis
+
+systemctl enable --now redis
+
+# Enable Redis Service to listen on all interfaces - By default, Redis service listens on 127.0.0.1.
+ss -tunelp | grep 6379
+
+# Accepter les connections remotes
+sed -i -e 's/bind 127.0.0.1/bind 0.0.0.0/g'  /etc/redis.conf    
+# Pour voir la config en masquant les commentaires:    grep ^[^#]  /etc/redis.conf
+
+
+systemctl  restart redis
+systemctl status redis
+
+
+#redis-cli 
+#127.0.0.1:6379> ping
+#PONG
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Load Redis environment variables
+. /opt/bitnami/scripts/redis-env.sh
+
+# Load libraries
+. /opt/bitnami/scripts/libbitnami.sh
+. /opt/bitnami/scripts/libredis.sh
+
+
+print_welcome_page
+
+if [[ "$*" = *"/opt/bitnami/scripts/redis/run.sh"* || "$*" = *"/run.sh"* ]]; then
+    info "** Starting Redis setup **"
+    /opt/bitnami/scripts/redis/setup.sh
+    info "** Redis setup finished! **"
+fi
+
+echo ""
+exec "$@"
+
+
+
+
+grep -rnl /* -e 'Cats'
+	/bitnami/redis/data/appendonly.aof
+	/bitnami/redis/data/dump.rdb
+
+
+
+
+
