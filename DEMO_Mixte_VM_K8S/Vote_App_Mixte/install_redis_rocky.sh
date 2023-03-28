eb3d3456f9166eb60e2395fc82348bceda75b226
ahugla <ahugla@vmware.com>
2023-03-28 19:38:19 +0200

    Update install_redis_rocky.sh

--- DEMO_Mixte_VM_K8S/Vote_App_Mixte/install_redis_rocky.sh
+++ DEMO_Mixte_VM_K8S/Vote_App_Mixte/install_redis_rocky.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+# PARAMETERS
+# ----------
+WAVEFRONT_PROXY=e7788df6e9d3                    # ID du container docker si le proxy est en container
+echo "WAVEFRONT_PROXY = " $WAVEFRONT_PROXY
+
+
+
+# INSTALL AND CONFIGURE REDIS
+# ---------------------------
+
+cd /tmp
+
 #dnf module list redis
 dnf -y install redis
 
@@ -26,33 +38,6 @@ systemctl status redis
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Load Redis environment variables
 . /opt/bitnami/scripts/redis-env.sh
 
@@ -72,12 +57,70 @@ fi
 echo ""
 exec "$@"
 
-
-
-
 grep -rnl /* -e 'Cats'
 	/bitnami/redis/data/appendonly.aof
 	/bitnami/redis/data/dump.rdb
+
+
+
+
+
+
+# INSTALL TELEGRAF & ENABLE MONITORING BY WAVEFRONT
+# -------------------------------------------------
+
+cd /tmp
+
+# ajout du repo
+cat <<EOF | sudo tee /etc/yum.repos.d/influxdata.repo
+[influxdata]
+name = InfluxData Repository - Stable
+baseurl = https://repos.influxdata.com/stable/\$basearch/main
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdata-archive_compat.key
+EOF
+
+dnf repolist
+
+dnf install -y telegraf
+
+# config pour redis
+cat <<EOF > /etc/telegraf/telegraf.d/redis.conf
+[[inputs.redis]]
+  ## specify servers via a url matching:
+  ##  [protocol://][:password]@address[:port]
+  ##  e.g.
+  ##    tcp://localhost:6379
+  ##    tcp://:password@192.168.99.100
+  ##
+  ## If no servers are specified, then localhost is used as the host.
+  ## If no port is specified, 6379 is used
+  servers = ["tcp://`hostname -f`:6379"]
+EOF
+
+# config pour wavefront
+echo " " >> /etc/telegraf/telegraf.conf
+echo " " >> /etc/telegraf/telegraf.conf
+echo " " >> /etc/telegraf/telegraf.conf
+echo "# CONFIG WAVEFRONT"    >> /etc/telegraf/telegraf.conf
+echo "[[outputs.wavefront]]" >> /etc/telegraf/telegraf.conf
+echo "  host = \"vra-008178.cpod-vrealize.az-fkd.cloud-garage.net\"   #  proxy URL"  >> /etc/telegraf/telegraf.conf
+echo "  port = 2878                                                   #  proxy port" >> /etc/telegraf/telegraf.conf
+echo "  prefix = \"\"                                                 #  optionnel"  >> /etc/telegraf/telegraf.conf
+
+systemctl enable telegraf
+systemctl start telegraf
+
+
+
+
+
+
+
+
+
+
 
 
 
